3af2fbb650591bc1fd88725a734bbb376de12553
aditya-diems <126227537+aditya-diems@users.noreply.github.com>
2023-05-03 20:45:33 +0530

    Revert "Revert "Revert "Revert "home UI changed""""
    This reverts commit 04c0e065ea8a2689daf58146b5196992a8cc3970.

---
+++ homeinfo.py
@@ -0,0 +1,182 @@
+import pandas as pd
+from routes import mysql_stud
+import mysql.connector
+
+
+def getleccount(subs):
+    at_btech = mysql.connector.connect(
+        user='root', password='', host='localhost', database='theory_btech')
+    at_ty = mysql.connector.connect(
+        user='root', password='', host='localhost', database='theory_ty')
+    at_sy = mysql.connector.connect(
+        user='root', password='', host='localhost', database='theory_sy')
+    ap_btech = mysql.connector.connect(
+        user='root', password='', host='localhost', database='practical_btech')
+    ap_ty = mysql.connector.connect(
+        user='root', password='', host='localhost', database='practical_ty')
+    ap_sy = mysql.connector.connect(
+        user='root', password='', host='localhost', database='practical_sy')
+
+    btech = at_btech.cursor()
+    ty = at_ty.cursor()
+    sy = at_sy.cursor()
+
+    btechP = ap_btech.cursor()
+    tyP = ap_ty.cursor()
+    syP = ap_sy.cursor()
+
+    count = {}
+    for i in subs:
+        for j in subs[i]:
+            for k in subs[i][j]:
+                for l in subs[i][j][k]:
+                    if i == 'BTECH':
+                        if k == 'THEORY':
+                            sql = "SHOW COLUMNS FROM `{}`".format(l)
+                            btech.execute(sql)
+                            cols = btech.fetchall()
+                            for tit in range(len(cols)):
+                                cols[tit] = cols[tit][0]
+                            cols = cols[3:]
+                            tempcount = 0
+                            for fig in cols:
+                                sql = "SELECT `{}` from `{}` WHERE `division`='{}'".format(
+                                    fig, l, j)
+                                btech.execute(sql)
+                                data = btech.fetchall()
+                                for zz in range(len(data)):
+                                    data[zz] = data[zz][0]
+                                if 1 or 0 in data:
+                                    tempcount += 1
+                            if 'other attendance' not in l:
+                                if j in count:
+                                    count[j][l] = tempcount
+                                else:
+                                    count[j] = {}
+                                    count[j][l] = tempcount
+
+                        elif k == 'PRACTICAL':
+                            sql = "SHOW COLUMNS FROM `{}`".format(l)
+                            btechP.execute(sql)
+                            cols = btechP.fetchall()
+                            for tit in range(len(cols)):
+                                cols[tit] = cols[tit][0]
+                            cols = cols[4:]
+                            tempcount = 0
+                            for fig in cols:
+                                sql = "SELECT `{}` from `{}` WHERE `division`='{}'".format(
+                                    fig, l, j)
+                                btechP.execute(sql)
+                                data = btechP.fetchall()
+                                for zz in range(len(data)):
+                                    data[zz] = data[zz][0]
+                                if 1 or 0 in data:
+                                    tempcount += 1
+                            if j in count:
+                                count[j][l] = tempcount
+                            else:
+                                count[j] = {}
+                                count[j][l] = tempcount
+
+                    elif i == 'TY':
+                        if k == 'THEORY':
+                            sql = "SHOW COLUMNS FROM `{}`".format(l)
+                            ty.execute(sql)
+                            cols = ty.fetchall()
+                            for tit in range(len(cols)):
+                                cols[tit] = cols[tit][0]
+                            cols = cols[3:]
+                            tempcount = 0
+                            for fig in cols:
+                                sql = "SELECT `{}` from `{}` WHERE `division`='{}'".format(
+                                    fig, l, j)
+                                ty.execute(sql)
+                                data = ty.fetchall()
+                                for zz in range(len(data)):
+                                    data[zz] = data[zz][0]
+                                if 1 or 0 in data:
+                                    tempcount += 1
+                            if 'other attendance' not in l:
+                                if j in count:
+                                    count[j][l] = tempcount
+                                else:
+                                    count[j] = {}
+                                    count[j][l] = tempcount
+
+                        elif k == 'PRACTICAL':
+                            sql = "SHOW COLUMNS FROM `{}`".format(l)
+                            tyP.execute(sql)
+                            cols = tyP.fetchall()
+                            for tit in range(len(cols)):
+                                cols[tit] = cols[tit][0]
+                            cols = cols[4:]
+                            tempcount = 0
+                            for fig in cols:
+                                sql = "SELECT `{}` from `{}` WHERE `division`='{}'".format(
+                                    fig, l, j)
+                                tyP.execute(sql)
+                                data = tyP.fetchall()
+                                for zz in range(len(data)):
+                                    data[zz] = data[zz][0]
+                                if 1 or 0 in data:
+                                    tempcount += 1
+                            if j in count:
+                                count[j][l] = tempcount
+                            else:
+                                count[j] = {}
+                                count[j][l] = tempcount
+
+                    elif i == 'SY':
+                        if k == 'THEORY':
+                            sql = "SHOW COLUMNS FROM `{}`".format(l)
+                            sy.execute(sql)
+                            cols = sy.fetchall()
+                            for tit in range(len(cols)):
+                                cols[tit] = cols[tit][0]
+                            cols = cols[3:]
+                            tempcount = 0
+                            for fig in cols:
+                                sql = "SELECT `{}` from `{}` WHERE `division`='{}'".format(
+                                    fig, l, j)
+                                sy.execute(sql)
+                                data = sy.fetchall()
+                                for zz in range(len(data)):
+                                    data[zz] = data[zz][0]
+                                if 1 or 0 in data:
+                                    tempcount += 1
+                            if 'other attendance' not in l:
+                                if j in count:
+                                    count[j][l] = tempcount
+                                else:
+                                    count[j] = {}
+                                    count[j][l] = tempcount
+
+                        elif k == 'PRACTICAL':
+                            sql = "SHOW COLUMNS FROM `{}`".format(l)
+                            syP.execute(sql)
+                            cols = syP.fetchall()
+                            for tit in range(len(cols)):
+                                cols[tit] = cols[tit][0]
+                            cols = cols[4:]
+                            tempcount = 0
+                            for fig in cols:
+                                sql = "SELECT `{}` from `{}` WHERE `division`='{}'".format(
+                                    fig, l, j)
+                                syP.execute(sql)
+                                data = syP.fetchall()
+                                for zz in range(len(data)):
+                                    data[zz] = data[zz][0]
+                                if 1 or 0 in data:
+                                    tempcount += 1
+                            if j in count:
+                                count[j][l] = tempcount
+                            else:
+                                count[j] = {}
+                                count[j][l] = tempcount
+    at_btech.close()
+    at_sy.close()
+    at_ty.close()
+    ap_btech.close()
+    ap_sy.close()
+    ap_ty.close()
+    return count
